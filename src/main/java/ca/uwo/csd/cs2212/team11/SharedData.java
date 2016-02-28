1d3fcc3bec1d376a5177a5a9aa793ae4c49f9cf5
Dara Amin <drasoulm@uwo.ca>
2016-02-28 12:43:28 -0500

    Added file paths

--- src/main/java/ca/uwo/csd/cs2212/team11/SharedData.java
+++ src/main/java/ca/uwo/csd/cs2212/team11/SharedData.java
@@ -5,6 +5,6 @@ import java.awt.Color;
 public final class SharedData {
 	public static enum IDs{ CALORIES, DISTANCE, CLIMB, STEPS, ACTIVE, SEDENTARY, HEART_RATE};
 	public static final Color[] COLOR_SET = {Color.RED, Color.ORANGE, Color.YELLOW, Color.GREEN, Color.CYAN, Color.BLUE, Color.PINK};
-	public static final String PATH_TO_RESOURCES = "./src/main/resources/";
+	public static final String PATH_TO_RESOURCES = "../src/main/resources/";
 	public static Color SMOKE = new Color(120, 120, 120, 50);
 }
