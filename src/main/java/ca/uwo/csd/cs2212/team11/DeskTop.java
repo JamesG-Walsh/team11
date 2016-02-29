498120f389cf02bb5c6f96943cf49b5839a8a01e
Andrew Hall <ahall9@uwo.ca>
2016-02-29 17:40:50 -0500

    Restoring Sundays work

--- src/main/java/ca/uwo/csd/cs2212/team11/DeskTop.java
+++ src/main/java/ca/uwo/csd/cs2212/team11/DeskTop.java
@@ -8,6 +8,7 @@ import java.awt.Font;
 import java.awt.GridLayout;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 import javax.swing.Box;
@@ -25,10 +26,8 @@ public class DeskTop extends JFrame{
 	
 	private Widget[] all_widgets = new Widget[7];	
 	private boolean[] widgetVisible = {false, false, false, false, false, false, false};
-	private JPanel[] primaryDisplayAreas = new JPanel[4];
-	private JPanel overallPanel, goalsPanel, widgetPanel, datePanel;
-	private JLabel[] overallLabels = new JLabel[28];
-	private JLabel goalsListLabel, dateLabel;
+	private JPanel goalsPanel, widgetPanel, datePanel, northPanel, westPanel, awardsPanel, eastPanel, southPanel;
+	private JLabel goalsListLabel, dateLabel, awardsListLabel;
 	
 	public DeskTop(){
 		super("Team 11 FitBit Viewer");
@@ -41,62 +40,56 @@ public class DeskTop extends JFrame{
 		all_widgets[IDs.SEDENTARY.ordinal()] = new Widget(IDs.SEDENTARY);
 		all_widgets[IDs.HEART_RATE.ordinal()] = new Widget(IDs.HEART_RATE);
 		
-		ImagePanel backPanel = new ImagePanel(SharedData.PATH_TO_RESOURCES + "../src/main/resources/imgs/jogger.jpg"); // replace with no copyright
-		JLayeredPane main = new JLayeredPane();
+		ImagePanel backPanel = new ImagePanel("jogger.jpg"); // replace with no copyright
 		this.setSize(backPanel.getWidth(), backPanel.getHeight());
-		main.setSize(backPanel.getWidth(), backPanel.getHeight());
-		main.add(backPanel, -3000);
 
 		
-		JPanel mainDisplay = (JPanel) this.getGlassPane();
-		this.setGlassPane(mainDisplay);
-		mainDisplay.setLayout(new BoxLayout(mainDisplay, 1));
+		JPanel mainDisplay = new JPanel();
+		mainDisplay.setLayout(new BorderLayout(1,1));
 		mainDisplay.setOpaque(false);
 		mainDisplay.setVisible(true);
-		mainDisplay.setSize(main.getSize());
-		this.add(mainDisplay, 0);
 		
-		primaryDisplayAreas[0] = new JPanel();
-		primaryDisplayAreas[0].setLayout(new BoxLayout(primaryDisplayAreas[0], 1));
-		JPanel welcomePanel = new JPanel();
-		welcomePanel.setLayout(new BoxLayout(welcomePanel, BoxLayout.LINE_AXIS));
-		String temp = SharedData.PATH_TO_RESOURCES + "imgs/Fitbit.png";
-		ImagePanel fitBitPic = new ImagePanel(temp);
-		temp = null;
-		fitBitPic.setSize(fitBitPic.getWidth(), fitBitPic.getHeight());
-		fitBitPic.setAlignmentX(Component.CENTER_ALIGNMENT);
-		welcomePanel.add(fitBitPic);
-		welcomePanel.add(Box.createHorizontalGlue());
+		northPanel = new JPanel();
+		northPanel.setLayout(new BoxLayout(northPanel, BoxLayout.LINE_AXIS));
+		northPanel.setOpaque(false);
+
+			ImagePanel fitBitPic = new ImagePanel("Fitbit.png");
+			fitBitPic.setSize(fitBitPic.getWidth(), fitBitPic.getHeight());
+			fitBitPic.setAlignmentX(Component.CENTER_ALIGNMENT);
+			northPanel.add(fitBitPic);
+			northPanel.add(Box.createHorizontalGlue());
 		
-		JPanel hello = new JPanel();
-		hello.setBackground(SharedData.SMOKE);
-		JLabel helloLabel = new JLabel("Welcome, USER!");
-		helloLabel.setFont(new Font("Tahoma", Font.PLAIN, 36));
-		hello.add(helloLabel);
-		welcomePanel.add(hello);
-		welcomePanel.add(Box.createHorizontalGlue());
+			JPanel hello = new JPanel();
+			hello.setBackground(SharedData.SMOKE);
+			JLabel helloLabel = new JLabel("Welcome, USER!");
+			helloLabel.setFont(new Font("Tahoma", Font.PLAIN, 36));
+			hello.add(helloLabel);
+			northPanel.add(hello);
+			northPanel.add(Box.createHorizontalGlue());
 		
-		temp = SharedData.PATH_TO_RESOURCES + "/imgs/Settings.png";
-		ImagePanel settingsButton = new ImagePanel(temp);
-		temp = SharedData.PATH_TO_RESOURCES + "/imgs/Exit_Button.png";
-		ImagePanel exitButton = new ImagePanel(temp);
-		temp = null;
-		settingsButton.addMouseListener(new MouseAdapter(){
-			public void mouseClicked(MouseEvent e){
-				openSettingsPanel();
-			}
-		});
-		exitButton.addMouseListener(new MouseAdapter(){
-			public void mouseClicked(MouseEvent e){
-				System.exit(0);
-			}
-		});
-		welcomePanel.add(settingsButton);
-		welcomePanel.add(exitButton);
+			ImagePanel settingsButton = new ImagePanel("Settings.png");
+			settingsButton.addMouseListener(new MouseAdapter(){
+				public void mouseClicked(MouseEvent e){
+					openSettingsPanel();
+				}
+			});
+			northPanel.add(settingsButton);
+	
+			ImagePanel exitButton = new ImagePanel("Exit_Button.png");
+			exitButton.addMouseListener(new MouseAdapter(){
+				public void mouseClicked(MouseEvent e){
+					System.exit(0);
+				}
+			});
+			northPanel.add(exitButton);
 		
-		primaryDisplayAreas[0].add(welcomePanel);
+		mainDisplay.add(northPanel, BorderLayout.NORTH);
 		
-		JPanel buttonArray = new JPanel();
+			
+		westPanel = new JPanel();
+			JPanel calorButton = createNavButton(IDs.CALORIES, "C");
+			westPanel.add(calorButton);
+		westPanel.setOpaque(false);
 		JPanel caloriesButton = new JPanel();
 		caloriesButton.setBackground(SharedData.COLOR_SET[IDs.CALORIES.ordinal()]);
 		caloriesButton.add(new JLabel("C"));
@@ -106,8 +99,8 @@ public class DeskTop extends JFrame{
 				addRemoveWidget(IDs.CALORIES);
 			}
 		});
-		buttonArray.add(caloriesButton);
-		
+		westPanel.add(caloriesButton);
+
 		JPanel distanceButton = new JPanel();
 		distanceButton.setBackground(SharedData.COLOR_SET[IDs.DISTANCE.ordinal()]);
 		distanceButton.add(new JLabel("D"));
@@ -117,7 +110,7 @@ public class DeskTop extends JFrame{
 				addRemoveWidget(IDs.DISTANCE);
 			}
 		});
-		buttonArray.add(distanceButton);
+		westPanel.add(distanceButton);
 		
 		JPanel climbButton = new JPanel();
 		climbButton.setBackground(SharedData.COLOR_SET[IDs.CLIMB.ordinal()]);
@@ -128,7 +121,7 @@ public class DeskTop extends JFrame{
 				addRemoveWidget(IDs.CLIMB);
 			}
 		});
-		buttonArray.add(climbButton);
+		westPanel.add(climbButton);
 		
 		JPanel stepsButton = new JPanel();
 		stepsButton.setBackground(SharedData.COLOR_SET[IDs.STEPS.ordinal()]);
@@ -139,7 +132,7 @@ public class DeskTop extends JFrame{
 				addRemoveWidget(IDs.STEPS);
 			}
 		});
-		buttonArray.add(stepsButton);
+		westPanel.add(stepsButton);
 		
 		JPanel activeButton = new JPanel();
 		activeButton.setBackground(SharedData.COLOR_SET[IDs.ACTIVE.ordinal()]);
@@ -150,7 +143,7 @@ public class DeskTop extends JFrame{
 				addRemoveWidget(IDs.ACTIVE);
 			}
 		});
-		buttonArray.add(activeButton);
+		westPanel.add(activeButton);
 		
 		JPanel sedentaryButton = new JPanel();
 		sedentaryButton.setBackground(SharedData.COLOR_SET[IDs.SEDENTARY.ordinal()]);
@@ -161,7 +154,7 @@ public class DeskTop extends JFrame{
 				addRemoveWidget(IDs.SEDENTARY);
 			}
 		});
-		buttonArray.add(sedentaryButton);
+		westPanel.add(sedentaryButton);
 		
 		JPanel hRateButton = new JPanel();
 		hRateButton.setBackground(SharedData.COLOR_SET[IDs.HEART_RATE.ordinal()]);
@@ -172,96 +165,91 @@ public class DeskTop extends JFrame{
 				addRemoveWidget(IDs.HEART_RATE);
 			}
 		});
-		buttonArray.add(hRateButton);
-		primaryDisplayAreas[0].add(buttonArray);
+		westPanel.add(hRateButton);
 		
-		JPanel decoration = new JPanel();
-		decoration.setSize((int) (mainDisplay.getWidth() * 0.75), 10); // why doesn't width work?
-		decoration.setBackground(SharedData.SMOKE);
-		primaryDisplayAreas[0].add(decoration);
+		mainDisplay.add(westPanel, BorderLayout.WEST);
 		
-		mainDisplay.add(primaryDisplayAreas[0]);
+		eastPanel = new JPanel();
 		
-		primaryDisplayAreas[1] = new JPanel();
-		overallPanel = new JPanel();
-		overallPanel.setBackground(SharedData.SMOKE);
-//		SpringLayout oap_layout = new SpringLayout();
-//		overallPanel.setLayout(oap_layout);
-		overallPanel.setLayout(new GridLayout(4, 8));
-		for(int i = 0; i < overallLabels.length; i++){
-			overallLabels[i] = new JLabel("Blah");
-			overallPanel.add(overallLabels[i]);
-		}
-		overallLabels[0].setText("");
-		overallLabels[1].setText("Calories");
-		overallLabels[2].setText("Steps");
-		overallLabels[3].setText("Distance");
-		overallLabels[4].setText("Floors");
-		overallLabels[5].setText("Active");
-		overallLabels[6].setText("Sedentary");
-		overallLabels[7].setText("Today:");
-		overallLabels[7].setFont(new Font("Tahoma", Font.PLAIN, 18));
-		overallLabels[14].setText("Best:");
-		overallLabels[14].setFont(new Font("Tahoma", Font.PLAIN, 18));
-		overallLabels[21].setText("Lifetime:");
-		overallLabels[21].setFont(new Font("Tahoma", Font.PLAIN, 18));
-//		SpringUtilities.makeGrid(overallPanel, 4, 8, 5, 5, 5, 5);
-		primaryDisplayAreas[1].add(overallPanel);
+			goalsPanel = new JPanel();
+			goalsPanel.setBackground(SharedData.SMOKE);
+			goalsPanel.setLayout(new BoxLayout(goalsPanel,1));
+			JLabel goalsTitleLabel = new JLabel("Daily Goals");
+			goalsTitleLabel.setFont(new Font("Tahoma", Font.BOLD, 18));
+			goalsPanel.add(goalsTitleLabel);
+			goalsListLabel= new JLabel("<html>Blah<br/>Blah<br/>Blah<br/>Blah<br/>Blah<br/>Blah<br/>Blah<br/></html>");
+			goalsPanel.add(goalsListLabel);
+		eastPanel.add(goalsPanel);
 		
-		goalsPanel = new JPanel();
-		goalsPanel.setBackground(SharedData.SMOKE);
-		goalsPanel.setLayout(new BoxLayout(goalsPanel,1));
-		JLabel goalsTitleLabel = new JLabel("Daily Goals");
-		goalsTitleLabel.setFont(new Font("Tahoma", Font.BOLD, 18));
-		goalsPanel.add(goalsTitleLabel);
-		goalsListLabel= new JLabel("<html>Blah<br/>Blah<br/>Blah<br/>Blah<br/>Blah<br/>Blah<br/>Blah<br/></html>");
-		goalsPanel.add(goalsListLabel);
-		primaryDisplayAreas[1].add(goalsPanel);
-		mainDisplay.add(primaryDisplayAreas[1]);
+
+			awardsPanel = new JPanel();
+			awardsPanel.setBackground(SharedData.SMOKE);
+			awardsPanel.setLayout(new BoxLayout(awardsPanel,1));
+			JLabel awardsTitleLabel = new JLabel("Achievements");
+			awardsTitleLabel.setFont(new Font("Tahoma", Font.BOLD, 18));
+			awardsPanel.add(awardsTitleLabel);
+			awardsListLabel= new JLabel("<html>Blah<br/>Blah<br/>Blah<br/>Blah<br/>Blah<br/>Blah<br/>Blah<br/></html>");
+			awardsPanel.add(awardsListLabel);
+		eastPanel.add(awardsPanel);
 		
-		primaryDisplayAreas[2] = new JPanel();
+		mainDisplay.add(eastPanel, BorderLayout.EAST);
+
+		
+		
 		widgetPanel = new JPanel();
 		widgetPanel.setBackground(SharedData.SMOKE);
-		primaryDisplayAreas[2].add(widgetPanel);
-		mainDisplay.add(primaryDisplayAreas[2]);
+		mainDisplay.add(widgetPanel, BorderLayout.CENTER);
 		
 		
-		primaryDisplayAreas[3] = new JPanel();
-		datePanel = new JPanel();
-//		datePanel.setOpaque(false);
-//		datePanel.setLayout(new BoxLayout(datePanel, BoxLayout.LINE_AXIS));
-		FlowLayout dp_layout = new FlowLayout(FlowLayout.LEFT);
-//		dp_layout.putConstraints(5); for SpringLayout only
-		datePanel.setLayout(dp_layout);
-		datePanel.setBackground(SharedData.SMOKE);
-		dateLabel = new JLabel(new Date().toString());
-		dateLabel.setFont(new Font("Tahoma", Font.PLAIN, 36));
-		datePanel.add(dateLabel); /// *** should change format
-		temp = SharedData.PATH_TO_RESOURCES + "imgs/Refresh.png";
-		ImagePanel refreshButton = new ImagePanel(temp);
-		refreshButton.setAlignmentX(CENTER_ALIGNMENT); // doesn't seem to be working
-		refreshButton.addMouseListener(new MouseAdapter(){
-			public void mouseClicked(MouseEvent e){
-				refreshData();
-			}
-		});
-		datePanel.add(refreshButton); // refreshButton starts disappearing after adding addWidgetButton 
-		primaryDisplayAreas[3].add(datePanel);
-		primaryDisplayAreas[3].add(Box.createHorizontalGlue());
+		southPanel = new JPanel();
+		southPanel.setLayout(new BoxLayout(southPanel, BoxLayout.LINE_AXIS));
+		southPanel.add(Box.createHorizontalBox());
 		
-		temp = SharedData.PATH_TO_RESOURCES + "imgs/Add_Widgit.png";
-		ImagePanel addWidgetButton = new ImagePanel(temp);
-		addWidgetButton.setAlignmentX(CENTER_ALIGNMENT); // doesn't seem to be working
-		addWidgetButton.addMouseListener(new MouseAdapter(){
-			public void mouseClicked(MouseEvent e){
-				addWidgetPanel();
-			}
-		});
-		primaryDisplayAreas[3].add(addWidgetButton);
+			datePanel = new JPanel();
+			datePanel.setBackground(SharedData.SMOKE);
+			String dateString = new SimpleDateFormat("yyyy-MM-dd").format(getDateOfLastRefresh());
+			dateLabel = new JLabel(dateString);
+			dateLabel.setFont(new Font("Tahoma", Font.PLAIN, 36));
+			datePanel.add(dateLabel);
+			
+			ImagePanel refreshButton = new ImagePanel("Refresh.png");
+			refreshButton.addMouseListener(new MouseAdapter(){
+				public void mouseClicked(MouseEvent e){
+					refreshData();
+				}
+			});
+			datePanel.add(refreshButton); // refreshButton starts disappearing after adding addWidgetButton 
+			
+			
+		southPanel.add(datePanel);
+		southPanel.add(Box.createHorizontalGlue());
+	
+			ImagePanel addWidgetButton = new ImagePanel("Add_Widgit.png");
+			addWidgetButton.addMouseListener(new MouseAdapter(){
+				public void mouseClicked(MouseEvent e){
+					addWidgetPanel();
+				}
+			});
+			southPanel.add(addWidgetButton);
 		
-		mainDisplay.add(primaryDisplayAreas[3]);
+		mainDisplay.add(southPanel, BorderLayout.SOUTH);
+		mainDisplay.setSize(backPanel.getSize());
+		backPanel.add(mainDisplay);
+		this.add(backPanel);
 	}
 	
+	private JPanel createNavButton(IDs type, String tag) {
+		JPanel a = new JPanel();
+		a.setBackground(SharedData.COLOR_SET[type.ordinal()]);
+		a.add(new JLabel(tag));
+		a.addMouseListener(new MouseAdapter(){
+			public void mouseClicked(MouseEvent e){
+				addRemoveWidget(IDs.CALORIES);
+			}
+		});
+		return a;
+	}
+
 	private void refreshData(){
 		System.err.println("DeskTop.refreshData() called");
 		System.err.println("\t***Does nothing yet");
@@ -288,6 +276,9 @@ public class DeskTop extends JFrame{
 		}
 	}
 	
+	private Date getDateOfLastRefresh(){
+		return new Date();
+	}
 	
 	private void openSettingsPanel(){
 		System.err.println("DeskTop.openSettingsPanel() called");
--- src/main/java/ca/uwo/csd/cs2212/team11/SharedData.java
+++ src/main/java/ca/uwo/csd/cs2212/team11/SharedData.java
@@ -6,5 +6,11 @@ public final class SharedData {
 	public static enum IDs{ CALORIES, DISTANCE, CLIMB, STEPS, ACTIVE, SEDENTARY, HEART_RATE};
 	public static final Color[] COLOR_SET = {Color.RED, Color.ORANGE, Color.YELLOW, Color.GREEN, Color.CYAN, Color.BLUE, Color.PINK};
 	public static final String PATH_TO_RESOURCES = "../src/main/resources/";
-	public static Color SMOKE = new Color(120, 120, 120, 50);
+	public static final String PATH_TO_IMAGES = "./src/main/resources/imgs/";
+	public static final String ALT_PATH_TO_IMAGES = "../src/main/resources/imgs/";
+	public static Color SMOKE = new Color(0, 0, 0, 100);
+	public static int[] base_array = {1492, 55555, 987654321};
+	public static int[] time_series = {126, 83, 118, 53, 80, 51, 83, 144, 98, 115, 134, 77, 129, 92, 55, 141, 149, 121, 60, 107, 109, 50, 131, 130, 122, 124, 117, 97, 61, 115, 102, 125, 92, 146, 99, 65, 140, 84, 107, 120, 69, 112, 89, 109, 83, 64, 106, 135, 104, 148,
+									126, 83, 118, 53, 80, 51, 83, 144, 98, 115, 134, 77, 129, 92, 55, 141, 149, 121, 60, 107, 109, 50, 131, 130, 122, 124, 117, 97, 61, 115, 102, 125, 92, 146, 99, 65, 140, 84, 107, 120, 69, 112, 89, 109, 83, 64, 106, 135, 104, 148,
+									126, 83, 118, 53, 80, 51, 83, 144, 98, 115, 134, 77, 129, 92, 55, 141, 149, 121, 60, 107, 109, 50, 131, 130, 122, 124, 117, 97, 61, 115, 102, 125, 92, 146, 99, 65, 140, 84, 107, 120, 69, 112, 89, 109, 83, 64, 106, 135, 104, 148} ;
 }
