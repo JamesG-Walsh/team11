bc3affeec6c89cb45c378e33f66d8454ccc0474f
Andrew Hall <ahall9@uwo.ca>
2016-03-13 15:25:30 -0400

    Widget now stays one size

--- src/main/java/ca/uwo/csd/cs2212/team11/Widget.java
+++ src/main/java/ca/uwo/csd/cs2212/team11/Widget.java
@@ -3,10 +3,12 @@ package ca.uwo.csd.cs2212.team11;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Component;
+import java.awt.Dimension;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
 import javax.swing.BorderFactory;
+import javax.swing.BoxLayout;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
@@ -29,6 +31,7 @@ public class Widget extends JPanel{
 	private JLabel hintLabel, viewLabel;
 	private JTextField dataBox = new JTextField(10);
 	private int[] data;
+	Color backColor;
 	
 	/**
 	 * Widget class constructor
@@ -36,12 +39,14 @@ public class Widget extends JPanel{
 	 */
 	public Widget(IDs type){
 		super();
-		this.setSize(200, 200);
-		System.out.println(this.getWidth());
-		this.setLayout(new BorderLayout(1,1));
-		this.setBackground(SharedData.COLOR_SET[type.ordinal()]);
-		this.setBorder(BorderFactory.createLineBorder(SharedData.COLOR_SET[type.ordinal()].darker()));
-
+		JPanel content = new JPanel();
+		this.setLayout(new BoxLayout(this, 1));
+		this.setPreferredSize(new Dimension(150, 150));
+		content.setLayout(new BorderLayout(1,1));
+		content.setBackground(SharedData.COLOR_SET[type.ordinal()]);
+		content.setBorder(BorderFactory.createLineBorder(SharedData.COLOR_SET[type.ordinal()].darker()));
+		this.add(content);
+		
 		switch(type){
 			case CALORIES:
 				this.typeName = "Calories Burned";
@@ -78,11 +83,11 @@ public class Widget extends JPanel{
 				typeName = "Undefined Widget";
 		}
 		
-		this.add(new JLabel(typeName), BorderLayout.NORTH);
-		hintLabel = new JLabel("Click Widget to Change View");
+		content.add(new JLabel(typeName), BorderLayout.NORTH);
+		hintLabel = new JLabel("Click to Change View");
 		viewLabel = new JLabel();
 		dataBox.setEditable(false);
-		dataBox.setBackground(new Color(255, 255, 255, 100));
+		dataBox.setOpaque(false);
 		dataBox.addMouseListener(new MouseAdapter(){
 			public void mouseClicked(MouseEvent e){
 				Component source = (Component)e.getSource();
@@ -90,7 +95,7 @@ public class Widget extends JPanel{
 			}
 		});
 
-		this.add(hintLabel, BorderLayout.SOUTH);
+		content.add(hintLabel, BorderLayout.SOUTH);
 		//changeView(0);
 		switch(type){
 			case CALORIES:
@@ -124,9 +129,9 @@ public class Widget extends JPanel{
 			default:
 				typeName = "Undefined Widget";
 		}
-		this.add(viewLabel, BorderLayout.WEST);
-		this.add(dataBox, BorderLayout.CENTER);
-		this.addMouseListener(new MouseAdapter(){
+		content.add(viewLabel, BorderLayout.WEST);
+		content.add(dataBox, BorderLayout.CENTER);
+		content.addMouseListener(new MouseAdapter(){
 			public void mouseClicked(MouseEvent e){
 				currentView = (currentView + 1) % maxView;
 				changeView(currentView);
@@ -135,6 +140,7 @@ public class Widget extends JPanel{
 
 	}
 	
+
 	/**
 	 * change view to specific data
 	 * @param i the index of a given data type
@@ -154,9 +160,9 @@ public class Widget extends JPanel{
 		return SharedData.base_array;
 	}
 	
-	/*private int[] getStepsData(IDs type) {
+	private int[] getStepsData(IDs type) {
 		return SharedData.steps_Data;
-	}*/
+	}
 	
 	/**
 	 * Get sedentary canned data
