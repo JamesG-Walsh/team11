de22192c3c254e8e3a6016a895936242fdfe1a17
Andrew Hall <ahall9@uwo.ca>
2016-03-21 15:28:33 -0400

    My version of the Widget

--- src/main/java/ca/uwo/csd/cs2212/team11/Widget.java
+++ src/main/java/ca/uwo/csd/cs2212/team11/Widget.java
@@ -1,21 +1,16 @@
 package ca.uwo.csd.cs2212.team11;
 
 import java.awt.BorderLayout;
-import java.awt.Color;
 import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import java.io.Serializable;
+
 import javax.swing.BorderFactory;
 import javax.swing.BoxLayout;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.Date;
-
 import ca.uwo.csd.cs2212.team11.SharedData.IDs;
 
 /**
@@ -23,281 +18,139 @@ import ca.uwo.csd.cs2212.team11.SharedData.IDs;
  * @author Andrew Hall
  * 
  */
-public class Widget extends JPanel implements Serializable{
-	/*private static final String[] views = {"<html>D<br/>a<br/>i<br/>l<br/>y</html>",
-		"<html>R<br/>e<br/>c<br/>o<br/>r<br/>d</html>", 
-		"<html>L<br/>i<br/>f<br/>e<br/>t<br/>i<br/>m<br/>e</html>"};*/
-	private static final String[] views = {"<html>Daily</html>",
-		"<html>Record</html>", 
-		"<html>Lifetime</html>"};
+public class Widget extends JPanel{
+	private static final String[] views = {"<html>D<br/>a<br/>i<br/>l<br/>y</html>",
+											"<html>R<br/>e<br/>c<br/>o<br/>r<br/>d</html>", 
+											"<html>L<br/>i<br/>f<br/>e<br/>t<br/>i<br/>m<br/>e</html>"};
 	private int goals = 0;
 	private int currentView = 0;
 	private int maxView = 3;
-	private String units, typeName;
+	private String units, typeName, altUnit, stash;
 	private JLabel hintLabel, viewLabel;
 	private JTextField dataBox = new JTextField(10);
 	private int[] data;
-	private IDs typeLive;
-	private User user;
-	private Calendar calen;
-
-	Color backColor;
 	
 	/**
 	 * Widget class constructor
 	 * @param type	the type of the widget
 	 */
-	public Widget(User usr, Calendar cal, IDs type){
+	public Widget(IDs type){
 		super();
-
-		user = usr;
-		calen = cal;
-		typeLive = type;
-
-		System.out.println(typeLive);
-		JPanel content = new JPanel();
 		this.setLayout(new BoxLayout(this, 1));
-		this.setPreferredSize(new Dimension(150, 150));
-		content.setLayout(new BorderLayout(1,1));
-		content.setBackground(SharedData.COLOR_SET[type.ordinal()]);
-		content.setBorder(BorderFactory.createLineBorder(SharedData.COLOR_SET[type.ordinal()].darker()));
-		this.add(content);
-
+		this.setPreferredSize(new Dimension(200, 200));
+		JPanel display = new JPanel();
+		display.setLayout(new BorderLayout(1,1));
+		display.setBackground(SharedData.COLOR_SET[type.ordinal()]);
+		
+		//this.setBackground(new Color(0,0,0,0));
+		display.setBorder(BorderFactory.createLineBorder(SharedData.COLOR_SET[type.ordinal()].darker()));
+		this.add(display);
+		
 		switch(type){
 			case CALORIES:
 				this.typeName = "Calories Burned";
 				this.units = "calories";
+				this.maxView = 2;
+				data = getData(type);
+				this.altUnit = "Joules";
 				break;
 			case DISTANCE:
 				this.typeName = "Distance Travelled";
-				this.units = "yards";
+				this.units = "km";
+				data = getDistanceData(type);
+				this.altUnit = "miles";
 				break;
 			case CLIMB:
 				this.typeName = "Floors Climbed";
 				this.units = "Floors";
+				data = getFloorsData(type);
 				break;
 			case STEPS:
 				this.typeName = "Steps Taken";
 				this.units = "steps";
+				data = getData(type);
 				break;
 			case ACTIVE:
 				this.typeName = "Minutes of Activity";
 				this.units = "minutes";
+				this.maxView = 2;
+				data = getActiveMinData(type);
 				break;
 			case SEDENTARY:
 				this.typeName = "Minute of Inactivity";
 				this.units = "minutes";
+				this.maxView = 2;
+				data = getSedData(type);
 				break;
 			case HEART_RATE:
 				this.typeName = "Heart Rate";
 				this.units = "bpm";
+				this.maxView = 1;
+				data = getData(type);
 				break;
 			default:
 				typeName = "Undefined Widget";
 		}
 		
-		content.add(new JLabel(typeName), BorderLayout.NORTH);
-		hintLabel = new JLabel("Click to Change View");
+		display.add(new JLabel(typeName), BorderLayout.NORTH);
+		hintLabel = new JLabel("Single view Widget");
+		if (this.maxView > 1){		hintLabel.setText("Click Widget to Change View"); 	}
 		viewLabel = new JLabel();
 		dataBox.setEditable(false);
-
 		dataBox.setOpaque(false);
+//		dataBox.setBackground(new Color(0, 0, 0)); does someone want this changed to black???
 		dataBox.addMouseListener(new MouseAdapter(){
 			public void mouseClicked(MouseEvent e){
 				Component source = (Component)e.getSource();
-				source.getParent().dispatchEvent(e);
+				if(e.getButton() == MouseEvent.BUTTON1){		source.getParent().dispatchEvent(e);	} 	
 			}
 		});
 
-		content.add(hintLabel, BorderLayout.SOUTH);
-		//changeView(0);
-		switch(type){
+		display.add(hintLabel, BorderLayout.SOUTH);
+		changeView(0);
+/*		switch(type){
 			case CALORIES:
-				data = getData(type);
-				changeView(0);
+				
 				break;
 			case DISTANCE:
-				if(Team11_FitBitViewer.testFlag){
-					data = getDistanceData(type);
-					changeView(0);
-				}else{
-					changeViewLive(user.getHistoricalFitnessData(), calen, 0, type);
-
-				}
+				changeView(0);
 				break;
 			case CLIMB:
-				if(Team11_FitBitViewer.testFlag){
-					data = getFloorsData(type);
-					changeView(0);
-				}else{
-					changeViewLive(user.getHistoricalFitnessData(), calen, 0, type);
-				}
+				changeView(0);
 				break;
 			case STEPS:
-				if(Team11_FitBitViewer.testFlag){
-					data = getData(type);
-					changeView(0);	
-				}else{
-					changeViewLive(user.getHistoricalFitnessData(), calen, 0, type);
-
-				}
-					
+				changeView(0);
 				break;
 			case ACTIVE:
-				if(Team11_FitBitViewer.testFlag){
-					data = getData(type);
-					changeView(0);
-				}else{
-					changeViewLive(user.getHistoricalFitnessData(), calen, 0, type);
-
-				}
+				changeView(0);
 				break;
 			case SEDENTARY:
-				if(Team11_FitBitViewer.testFlag){
-					data = getSedData(type);
-					changeView(0);
-				}else{
-					changeViewLive(user.getHistoricalFitnessData(), calen, 0, type);
-
-				}
+				changeView(0);
 				break;
 			case HEART_RATE:
-				data = getData(type);
 				changeView(0);
 				break;
 			default:
 				typeName = "Undefined Widget";
-		}
-		content.add(viewLabel, BorderLayout.WEST);
-		content.add(dataBox, BorderLayout.CENTER);
-		content.addMouseListener(new MouseAdapter(){
+		}*/
+		display.add(viewLabel, BorderLayout.WEST);
+		display.add(dataBox, BorderLayout.CENTER);
+		display.addMouseListener(new MouseAdapter(){
 			public void mouseClicked(MouseEvent e){
-				
-				//source.getParent().repaint();
-				// source.getParent().revalidate();
-				/*dataBox.revalidate();
-				dataBox.repaint();*/
-				if(Team11_FitBitViewer.testFlag){
-
-					currentView = (currentView + 1) % maxView;
-					changeView(currentView);
-				}else{
-
-					currentView = (currentView + 1) % maxView;
-					System.out.println(currentView);
-					changeViewLive(user.getHistoricalFitnessData(), calen, currentView, typeLive);
-				}
-
-				revalidate();
-				repaint();
+				currentView = (currentView + 1) % maxView;
+				changeView(currentView);
 			}
 		});
 
 	}
 	
-
 	/**
 	 * change view to specific data
 	 * @param i the index of a given data type
 	 */     
 	private void changeView(int i) {
-		
-		System.out.println("currentView :" + i);
 		dataBox.setText(this.data[i] + " " + this.units);
 		viewLabel.setText(Widget.views[i]);
-	}
-
-	public void changeViewLive(HistoricalFitnessData hfd, Calendar cal, int i, IDs type){
-
-		int year = cal.get(Calendar.YEAR);
-		int month = cal.get(Calendar.MONTH);
-		int day = cal.get(Calendar.DAY_OF_MONTH);
-
-		if(!Team11_FitBitViewer.testFlag){
-			OneDaysWorthOfData odwod = hfd.retrieve2(day, month+1, year);
-			System.out.println("Inside CVL...\n" + odwod.toString(false));
-			System.out.println("CVL hfd...\n" + hfd.lifetimeAndBestDaysToString());
-			System.out.println("i = " + i);
-
-		
-
-				switch(type){
-						case STEPS: 
-									System.out.print("Getting steps...");
-									if(i==0){ //Get day calorie
-										String convert = String.valueOf(odwod.getTodaysTotalSteps());
-										System.out.println(convert);
-										dataBox.setText(convert);
-										viewLabel.setText(Widget.views[i]);
-	
-									}else if(i==1){ //Get Best Day calorie
-										String convert = String.valueOf(hfd.getBestStepsValue());
-										//System.out.println(Team11_FitBitViewer.odwod.getTodaysTotalSteps());
-	
-										dataBox.setText(convert);
-										viewLabel.setText(Widget.views[i]);
-	
-									}else if(i==2){
-										String convert = String.valueOf(hfd.getLifetimeSteps());
-										dataBox.setText(convert);
-										viewLabel.setText(Widget.views[i]);
-									}
-									break;
-						case DISTANCE: 
-									if(i==0){ //Get day calorie
-										String convert = String.valueOf(odwod.getTodaysTotalDistance());
-										dataBox.setText(convert);
-										viewLabel.setText(Widget.views[i]);
-	
-									}else if(i==1){ //Get Best Day calorie
-										String convert = String.valueOf(hfd.getBestDistanceValue());
-										dataBox.setText(convert);
-										viewLabel.setText(Widget.views[i]);
-	
-									}else if(i==2){
-										String convert = String.valueOf(hfd.getLifetimeDistance());
-										dataBox.setText(convert);
-										viewLabel.setText(Widget.views[i]);
-									}
-									break;
-						case CLIMB: 
-									if(i==0){ //Get day calorie
-										String convert = String.valueOf(odwod.getTodaysTotalFloors());
-										System.out.println("CONVERT:- " + convert);
-										System.out.println("ODWOD TOTAL FLOORS - " + odwod.getTodaysTotalFloors());
-										dataBox.setText(convert);
-										viewLabel.setText(Widget.views[i]);
-		
-									}else if(i==1){ //Get Best Day calorie
-										String convert = String.format("%.2f",hfd.getBestFloorsValue());
-										dataBox.setText(convert);
-										viewLabel.setText(Widget.views[i]);
-		
-									}else if(i==2){
-										String convert = String.valueOf(Math.round(hfd.getLifetimeFloors()));
-										dataBox.setText(convert);
-										viewLabel.setText(Widget.views[i]);
-									}
-									break;
-						case ACTIVE: 
-							if(i==0){ //Get day calorie
-								String convert = String.valueOf(odwod.getTodaysTotalActiveMins());
-								dataBox.setText(convert);
-								viewLabel.setText(Widget.views[i]);
-
-							}
-							break;
-						case SEDENTARY: 
-							if(i==0){ //Get day calorie
-								String convert = String.valueOf(odwod.getTodaysTotalSedentaryMins());
-								dataBox.setText(convert);
-								viewLabel.setText(Widget.views[i]);
-							}
-							break;
-						default:
-							break;
-					}
-				}
 	}
 
 	/**
@@ -309,16 +162,16 @@ public class Widget extends JPanel implements Serializable{
 		return SharedData.base_array;
 	}
 	
-	private int[] getStepsData(IDs type) {
+	/*private int[] getStepsData(IDs type) {
 		return SharedData.steps_Data;
-	}
+	}*/
 	
 	/**
 	 * Get sedentary canned data
 	 * @param type -- 
 	 * @return sedentary data
 	 */
-	private int[] getSedData(IDs type) {
+	private int[] getSedData(IDs type) { // what is the purpose of the arg????
 		return SharedData.sedentary_Data;
 	}
 	
