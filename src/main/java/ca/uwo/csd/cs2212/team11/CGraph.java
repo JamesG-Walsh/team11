bf45d4ab360e05360fe65048e60856a4b7b6ec6d
Andrew Hall <ahall9@uwo.ca>
2016-03-21 15:29:27 -0400

    Just the start of CGraph

---
+++ src/main/java/ca/uwo/csd/cs2212/team11/CGraph.java
@@ -0,0 +1,48 @@
+package ca.uwo.csd.cs2212.team11;
+
+import javax.swing.JPanel;
+
+import ca.uwo.csd.cs2212.team11.SharedData.IDs;
+
+public class CGraph extends JPanel{
+	
+	private IDs type;
+	private double[] data;
+	private int[] hourlyData;
+	private int[] qtrHourlyData;
+	private int legend = 575;
+	private static int graphWidth = 650;
+	private static int graphHeight = 200;
+	private int endOfGraph = 534;
+
+	public CGraph(IDs type){
+		this.type = type;
+		switch(type){
+		case CALORIES:
+			normalizeData(getCaloriesData());
+			break;
+		case STEPS:
+			normalizeData(getStepsData());
+			break;
+		case DISTANCE:
+			normalizeData(getDistanceData());
+			break;
+		default:
+			System.err.println("Error in graph creation " + type.name() + " is not recognized");
+		}
+
+	}
+
+	private double[] getCaloriesData(){
+		return SharedData.newBigD;
+	}
+
+	private double[] getStepsData(){
+		return SharedData.newBigD;
+	}
+	
+	private double[] getDistanceData(){
+		return SharedData.newBigD;
+	}
+
+}
